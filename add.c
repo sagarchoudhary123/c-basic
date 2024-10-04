138c74c6cd009afb7226dad8272e598d026efcf3
sagarchoudhary123 <ch.sagar1401@gmail.com>
2024-10-04 12:06:22 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the two number");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum is:%d",c);
+    return 0;
+    getch ();
+}
